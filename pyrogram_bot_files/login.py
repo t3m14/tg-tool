8ff7bd8530ed2e22b3b74e3e5ef4c7cdc19c38f1
Артём <78920456+t3m14@users.noreply.github.com>
2022-10-09 08:19:36 +0300

    Update login.py

--- pyrogram_bot_files/login.py
+++ pyrogram_bot_files/login.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
-API_ID = "14107984"
-API_KEY = "6507fadc1d76c1d8f3c0957690d9ec86"
+API_ID = ""
+API_KEY = ""
 def login_by_session(session_file):
     app = Client(session_file, API_ID, API_KEY, workdir="../")
     return app
